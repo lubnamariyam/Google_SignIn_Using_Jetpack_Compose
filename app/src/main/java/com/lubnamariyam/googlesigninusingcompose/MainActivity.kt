e7d597a5d84587984c0f814491be083bf7b99197
Lubna Mariyam <lubnamariyam@ajro.com>
2021-09-14 21:31:08 +0530

    Initial commit

---
+++ app/src/main/java/com/lubnamariyam/googlesigninusingcompose/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.lubnamariyam.googlesigninusingcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.lubnamariyam.googlesigninusingcompose.ui.theme.GoogleSignInUsingComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GoogleSignInUsingComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GoogleSignInUsingComposeTheme {
+
+    }
+}
